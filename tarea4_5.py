8c89927b04f26117c722e5b218f2465475891fdd
Polenka Torres Orozco Román <polenka57_otrora82@ciencias.unam.mx>
2025-04-11 14:46:09 -0600

    Descripción de los cambios realizados

--- tarea4_5.py
+++ tarea4_5.py
@@ -1,7 +1,7 @@
 import numpy as np  
 
 # Definir las posiciones de los peces  
-loc = np.array([  
+locs = np.array([  
     [0, 0, 0],  
     [1, 1, 2],  
     [0, 0, 0],  
@@ -22,8 +22,18 @@ print("Pesos de los peces:", weights)
 
 # Sistema para rastrear supervivencia de peces en celdas  
 celdas_ocupadas = {}  
-for pez in np.range(len(loc)):
-    i, j, k = loc[pez]
-    posicion = (i, j, k)
-    if posicion in celdas_ocupadas
-    
+for index, loc in enumerate(locs):
+    pos = tuple (loc)
+    if pos not in celdas_ocupadas:
+        celdas_ocupadas[pos] = []
+    celdas_ocupadas[pos].append(weights[index])
+
+sobreviven  = {}
+for posicion, weights_list in celdas_ocupadas.items():
+    max_weight = max(weights_list)
+    sobreviven [posicion] = max_weight
+
+print ("\n Peces sobrevivientes (posición y peso):")
+for posicion, weight in sobreviven.items ():
+    print(f"Posición: {posicion}, Peso: {weights}"
+          )
